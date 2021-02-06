093421c1a994819ea6ba4abe6d1b5105897a826b
Fernandes <bruno.fernandes@cgi.com>
2021-02-06 18:44:09 +0000

    Initial commit

---
+++ src/test/java/pt/bnurbit/coding/spring/SpringCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.bnurbit.coding.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
